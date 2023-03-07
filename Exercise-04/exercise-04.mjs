9fa5acce3656c75a69b5c77b577635de51b53ca3
Santino Di Gennaro <santinodg96@gmail.com>
2023-03-07 16:31:01 +0100

    exercise 05

--- Exercise-04/exercise-04.mjs
+++
@@ -1,3 +0,0 @@
-import randomNumber from "./module.mjs";
-
-randomNumber();
--- Exercise-04/module.mjs
+++
@@ -1,5 +0,0 @@
-function randomNumber(){
-    console.log(parseInt(Math.random()*43))
-};
-
-export default randomNumber;
