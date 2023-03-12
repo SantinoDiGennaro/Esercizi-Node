93b27b891603feb40228910c67f3910050197d98
Santino Di Gennaro <santinodg96@gmail.com>
2023-03-12 21:39:34 +0100

    exercise 13

--- Exercise-13/exercise-13.js
+++ Exercise-13/exercise-13.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ data: "This is data" });
+    res.send({ data: "This is data" });
 });
 
 module.exports = app;
