d7c5d4fcec5d2684e086cb757ce6470946db7cb2
Santino Di Gennaro <santinodg96@gmail.com>
2023-03-13 12:22:52 +0100

    exercise 14

--- Exercise-13/exercise-13.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.json({ data: "This is data" });
-});
-
-module.exports = app;
--- Exercise-13/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    testEvirorment: "node",
-    verbose: true,
-};
