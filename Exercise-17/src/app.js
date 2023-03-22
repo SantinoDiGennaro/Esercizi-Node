2f6824902c09a95f9dec69f7d41e9ce3d8a86b86
Santino Di Gennaro <santinodg96@gmail.com>
2023-03-22 09:37:36 +0100

    exercise 17

--- Exercise-17/src/app.js
+++ Exercise-17/src/app.js
@@ -5,9 +5,19 @@ import {PrismaClient} from "@prisma/client"
 const prisma = new PrismaClient()
 const app = express()
 
+app.use(express.json())
+
 app.get("/data",async(req,res)=>{
     const data = await prisma.database.findMany();
     res.json(data)
+})
+
+app.post("/data", async(req,res)=>{
+    const datas = req.body
+    const data = await prisma.database.create({
+        data: datas
+    })
+    res.status(201).json(data)
 })
 
 export default app
